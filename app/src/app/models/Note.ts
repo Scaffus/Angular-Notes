0accc3541ba6b7ad79eec6b03b238fc37797de6f
Scaffus <77048269+Scaffus@users.noreply.github.com>
2021-07-01 17:01:44 +0200

    can delete, remodelling a bit the ui for now, working on the edit part afterward

--- app/src/app/models/Note.ts
+++ app/src/app/models/Note.ts
@@ -2,4 +2,5 @@ export class Note {
   title:string | undefined;
   content:string | undefined;
   id:string | undefined;
+  is_favorite:boolean = false;
 }
--- app/src/app/note/note.component.ts
+++ app/src/app/note/note.component.ts
@@ -15,10 +15,11 @@ export class NoteComponent implements OnInit {
   newNote: Note = new Note();
   notes: Observable<any[]>;
 
-  noteCollection = this.fdb.collection('notes');
+  noteRef = this.fdb.collection('notes');
   // newNote = this.store.collection('note').valueChanges({ idField: 'id' });
 
-  hideme = [true];
+  hideme = [false];
+  hideNoteForm = true;
 
   constructor(private fdb: AngularFirestore) {
     this.notes = this.fdb.collection('notes').valueChanges();
@@ -26,11 +27,16 @@ export class NoteComponent implements OnInit {
 
   ngOnInit(): void {}
 
-  addNote() {
-    if (this.newNote.title || this.newNote.content != '') {
+  async addNote() {
+    if (this.newNote.title && this.newNote.content != '') {
       // this.notes.push(this.newNote);
       this.newNote.id = this.fdb.createId();
-      this.noteCollection.add(Object.assign({}, this.newNote));
+
+      this.noteRef.doc(this.newNote.id)
+      this.noteRef.doc(this.newNote.id).set(Object.assign({}, this.newNote))
+      // .catch((error) => { alert(error); });
+
+      console.log(this.newNote.id)
       // this.newNote = new Note();
 
       console.log(this.newNote);
@@ -38,8 +44,10 @@ export class NoteComponent implements OnInit {
   }
 
   async deleteNote(note: Note) {
-    await this.fdb.collection('notes').doc(note.id).delete()
-      .catch((error) => { alert(error); });
+    await this.noteRef.doc(note.id).delete()
+    .catch((error) => { alert(error); });
+    console.log(note.id)
   }
+
 }
 
